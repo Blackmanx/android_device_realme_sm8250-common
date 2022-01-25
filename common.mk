a5088e43926bc940a615c561263dc5794f2d5980
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    sm8250-common: Bring back Oplus vibrator

--- common.mk
+++ common.mk
@@ -431,7 +431,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.oplus
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
