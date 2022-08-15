23270dfb670b0aa5578d5ed2bfccf80bef449ffc
Chandu Dyavanapelli <chandudyavanapelli03@gmail.com>
2022-08-15 08:13:16 +0000

    sm8350-common: Add oplus camera & gallery support [1/2]
    * Camera repository: https://gitlab.com/crdroidandroid/proprietary_vendor_oneplus_camera
    
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -116,6 +116,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    libcamera2ndk_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
@@ -317,6 +318,26 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libstagefrighthw
+
+
+# OnePlus camera
+$(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
+    $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
+
+# OnePlus framework
+PRODUCT_PACKAGES += \
+    oplus-fwk.lahaina
+
+PRODUCT_BOOT_JARS += \
+    oplus-fwk.lahaina
+
+# OnePlus wrapper
+PRODUCT_BOOT_JARS += \
+    oplus-support-wrapper
 
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
