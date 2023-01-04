c2fca2d6ff13ff32967ccd623ae53494632e8426
Mashopy <eliasgheeraert@gmail.com>
2023-01-04 16:37:04 +0100

    sm8250-common: Set GENERIC_ODM_IMAGE to true

--- common.mk
+++ common.mk
@@ -266,6 +266,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+GENERIC_ODM_IMAGE := true
+
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
