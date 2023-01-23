9b1696fc889ff1ddd7f51b5bb1ed285628a5c1a8
cristianvaz <cristianvaz81993@gmail.com>
2023-01-23 16:18:26 +0100

    sm8250-common: Add Livedisplay 2.1 support

--- common.mk
+++ common.mk
@@ -258,6 +258,10 @@ PRODUCT_PACKAGES += \
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-service.oplus
+    
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
 PRODUCT_COPY_FILES += \
