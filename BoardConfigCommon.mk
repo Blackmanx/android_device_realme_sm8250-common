893dfb3e73e6ae4132cb81bd4daea290871e6140
cristianvaz <cristianvaz81993@gmail.com>
2023-01-23 16:18:26 +0100

    sm8250-common: Add Livedisplay 2.1 support

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,7 +97,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
+    $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
