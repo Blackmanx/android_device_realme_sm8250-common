137a4ec9b9caca746f62a13ed2554b12d591b258
Mashopy <eliasgheeraert@gmail.com>
2023-02-21 00:26:22 +0100

    sm8250-common: Import CarrierConfig from RMX3370_11.C.11
    Change-Id: Ic2d8d7a75121c29d33cff7f178debf37d19833c5

--- common.mk
+++ common.mk
@@ -331,6 +331,7 @@ $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    OPlusCarrierConfigResCommon \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
