12a76b16fc7b4de884da8303dd533630477d309c
Mashopy <eliasgheeraert@gmail.com>
2023-04-19 19:48:48 +0200

    sm8250-common: manifest: Remove android.hardware.memtrack
    Android memtrack HAL job purpose is to easier memory tracking, which is completely useless for us and follow what Qualcomm did since taro.
    
    Change-Id: Id706835d667478791693f120154f4c24a419fb2a

--- common.mk
+++ common.mk
@@ -136,8 +136,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
