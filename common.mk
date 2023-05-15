720379b0df44ab0c762a1c72c49e9e6d25b82c2e
Blackmanx <blackman@sajl.tk>
2023-05-15 16:17:48 +0200

    sm8250-common: Remove unneeded packages for kenvyra

--- common.mk
+++ common.mk
@@ -50,7 +50,6 @@ PRODUCT_PACKAGES += \
     libdynproc \
     libeffectproxy \
     libhdmiedid \
-    libhdmipassthru \
     libhfp \
     libldnhncr \
     libqcompostprocbundle \
@@ -104,9 +103,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.bluetooth.default
 
-PRODUCT_PACKAGES += \
-    libbthost_if
-
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2400
@@ -153,7 +149,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libqdMetaData \
     libsdmcore \
     libsdmutils \
@@ -197,9 +192,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss@2.1-impl-qti \
-    android.hardware.gnss@2.1-service-qti
+    android.hardware.gnss@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
@@ -465,9 +458,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     wpa_supplicant.conf
 
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay \
-    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
