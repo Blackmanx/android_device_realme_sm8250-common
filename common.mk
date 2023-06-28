a6ec581a16719dac226821548a378df0df228d35
Blackmanx <blackman@sajl.tk>
2023-06-28 17:09:42 +0200

    Revert "sm8250-common: Remove QTI Thermal"
    This reverts commit 6059369a3141c023bfb3d48f5f3e94e02b705d7e.

--- common.mk
+++ common.mk
@@ -425,6 +425,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oplus
