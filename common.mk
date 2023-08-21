5825d0bfa5d5cfa17bbfcc718f4a275768c2edb3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    porsche: Introduce DolbyManager
    - Converted Xiaomi dirac implementation based on observing
      Xiaomi sound effects app and daxservice.
    - Thanks to jhenrique09 for the hack from Xiaomi dirac parts
      "Pause/play music stream to get effects applied".
    
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

--- common.mk
+++ common.mk
@@ -175,6 +175,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Dolby Manager
+PRODUCT_PACKAGES += \
+    DolbyManager
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
