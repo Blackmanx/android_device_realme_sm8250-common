1533151c4743fed86905897a729a92abc3a19c02
Mashopy <eliasgheeraert@gmail.com>
2023-11-11 11:42:31 +0100

    sm8250-common: := > += for vintf manifest inclusion
    Change-Id: I4b3db14c11333fdc64f12f2eb9123c45d208cf5b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,12 +96,12 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 # HIDL
 TARGET_USES_CUSTOM_C2_MANIFEST := true
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
-    vendor/bliss/config/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := \
+    vendor/derp/config/device_framework_matrix.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/manifest.xml \
     $(COMMON_PATH)/configs/vintf/c2_manifest_vendor.xml
 
