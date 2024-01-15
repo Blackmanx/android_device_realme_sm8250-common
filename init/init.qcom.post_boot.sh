bbebdbff8d03060fe38ab54d335bb670fc6825c9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-15 01:59:21 +0100

    [PATCH] sm8250-common: init: Move audio-app cpuset to 1-2
    audio driver affines pm-qos requests to these cpus so it makes sense
    to use them for audio-app as well
    
    Change-Id: I61223db706042e215b1ab5ca9eb4562989e362f6
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -5755,6 +5755,7 @@ case "$target" in
 	echo 0-1 > /dev/cpuset/background/cpus
 	echo 0-3 > /dev/cpuset/system-background/cpus
     echo 0-3 > /dev/cpuset/restricted/cpus
+    echo 1-2 > /dev/cpuset/audio-app/cpus
     
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
